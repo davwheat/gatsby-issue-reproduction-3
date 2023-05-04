c789bbae9e5f9412bb8704e2bfd95663beee95cf
David Wheatley <david@davwheat.dev>
2023-05-04 16:52:57 +0100

    chore: disable TS plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,11 +5,9 @@ const config: GatsbyConfig = {
     title: `gatsby-mdx-test`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    // "gatsby-plugin-typescript",
     "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
